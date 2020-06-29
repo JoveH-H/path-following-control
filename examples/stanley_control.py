9b5fd10950f3f31406b6c95337ef676f3929a9d5
jove <964312262@qq.com>
2020-06-29 11:57:23 +0800

    更改加载目录

--- examples/stanley_control.py
+++ examples/stanley_control.py
@@ -1,5 +1,7 @@
+import os
 import sys
-sys.path.append("..")
+sys.path.append(os.path.join(
+    os.path.dirname(__file__), os.path.pardir).replace('\\', '/'))
 from model import particle
 from controller import stanley
 
