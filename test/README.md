65e6aae45318a2bfa60dfb6ccd6a4951cc7c1a37
jove <964312262@qq.com>
2020-07-04 16:30:24 +0800

    新增测试执行文件

---
+++ test/README.md
@@ -0,0 +1,7 @@
+# test
+
+这是一个关于项目测试的文件。
+
+## 模块
+
+1. [examples_test.py](./examples_test.py)：例程测试
---
+++ test/examples_test.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("python ./examples/particle_motion.py")
+os.system("python ./examples/positional_pid_control.py")
+os.system("python ./examples/incremental_pid_control.py")
+os.system("python ./examples/pure_pursuit_control.py")
+os.system("python ./examples/stanley_control.py")
+os.system("python ./examples/lqr_control.py")
