236390e056d619f62ca49823da1cccb7acb8867c
jove <964312262@qq.com>
2020-07-06 14:20:50 +0800

    更新测试方式为pytest

--- test/examples_test.py
+++ test/examples_test.py
@@ -1,8 +1,30 @@
+import pytest
 import os
 
-os.system("python ./examples/particle_motion.py")
-os.system("python ./examples/positional_pid_control.py")
-os.system("python ./examples/incremental_pid_control.py")
-os.system("python ./examples/pure_pursuit_control.py")
-os.system("python ./examples/stanley_control.py")
-os.system("python ./examples/lqr_control.py")
+
+def test_case1():
+    os.system("python ./examples/particle_motion.py")
+
+
+def test_case2():
+    os.system("python ./examples/positional_pid_control.py")
+
+
+def test_case3():
+    os.system("python ./examples/incremental_pid_control.py")
+
+
+def test_case4():
+    os.system("python ./examples/pure_pursuit_control.py")
+
+
+def test_case5():
+    os.system("python ./examples/stanley_control.py")
+
+
+def test_case6():
+    os.system("python ./examples/lqr_control.py")
+
+
+if __name__ == "__main__":
+    pytest.main()
